4082f34ba0e7b4dad25655dd42ff9fee16b38e9f
Javier Morales <eyjavi@gmail.com>
2025-07-27 13:31:37 +0200

    estructura archivos creada

---
+++ usuarios/src/app/features/User/user-delete-confirmation/user-delete-confirmation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-delete-confirmation',
+  imports: [],
+  templateUrl: './user-delete-confirmation.component.html',
+  styleUrl: './user-delete-confirmation.component.css'
+})
+export class UserDeleteConfirmationComponent {
+
+}
