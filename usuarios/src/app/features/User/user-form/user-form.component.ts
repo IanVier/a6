9665338b37806030426a65162584cbf7f7b71e13
Javier Morales <eyjavi@gmail.com>
2025-07-29 23:20:15 +0200

    userForm works

--- usuarios/src/app/features/User/user-form/user-form.component.ts
+++ usuarios/src/app/features/User/user-form/user-form.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css'
 })
 export class UserFormComponent {
+
+  userForm: FormGroup;
+
+  constructor() {
+    this.userForm = new FormGroup({
+      first_name: new FormControl("",[
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      last_name: new FormControl("",[
+        Validators.required
+      ]),
+      email: new FormControl("",[
+        Validators.required
+      ]),
+      image: new FormControl("",[
+        Validators.required
+      ])
+    }, [])
+
+  }
+
+onSubmit(){
+  console.log(this.userForm.value)
+}
 
 }
