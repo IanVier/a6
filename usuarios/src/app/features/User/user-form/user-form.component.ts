99f940d6be7011df413705af60c9964428140eea
Javier Morales <eyjavi@gmail.com>
2025-08-02 13:05:59 +0200

    user edit form completed

--- usuarios/src/app/features/User/user-form/user-form.component.ts
+++ usuarios/src/app/features/User/user-form/user-form.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { UsersService } from '../../../model/service/users.service';
+import { IUserData } from '../../../model/interfaces/iuser.interface';
 
 @Component({
   selector: 'app-user-form',
@@ -10,6 +13,9 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 export class UserFormComponent {
 
   userForm: FormGroup;
+  userId: string | null = null
+  private activateRoute = inject(ActivatedRoute)
+  private userService = inject(UsersService)
 
   constructor() {
 
@@ -26,16 +32,50 @@ export class UserFormComponent {
       ]),
       image: new FormControl("",[
         Validators.required
-      ])
+      ]),
+      _id: new FormControl('')
       }, [])
 
   }
 
-onSubmit(){
-  console.log(
-    this.userForm.value,
-    this.userForm.valid
-  )
+  onSubmit() {
+    if (this.userForm.valid) {
+      const userData: IUserData = this.userForm.value;
+
+      if (this.userId) {
+          this.userService.updateUser(this.userId, userData).subscribe({
+            next: (response) => {
+              console.log('Usuario actualizado:', response);
+            },
+            error: (error) => {
+              console.error('Error al actualizar el usuario:', error);
+            }
+          })
+      } else {
+        this.userService.createUser(userData).subscribe( {
+          next: (response) => {
+            console.log('Usuario creado:', response);
+          },
+          error: (error) => {
+            console.error('Error al crear el usuario:', error);
+          }
+        }
+        );
+      }
+    }
+  }
+
+ngOnInit() : void {
+  this.activateRoute.params.subscribe(params => {
+    this.userId = params['id'] || null
+    if (this.userId) {
+      this.userService.getById(this.userId).subscribe(user => {
+        this.userForm.patchValue(user)
+      })
+    }
+  })
 }
 
 }
+
+
